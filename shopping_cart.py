532c64bf3d548066239615801955c646febbc827
jkg363 <65503702+jkg363@users.noreply.github.com>
2020-06-13 22:11:14 -0400

    Stored lookups in list

--- shopping_cart.py
+++ shopping_cart.py
@@ -28,20 +28,22 @@ products = [
 # INFO CAPTURE / INPUT
 #
 total_price = 0
-
+selected_ids = []
 while True:
     selected_id = input("Please input a product identifier, or 'DONE' if there are no more items: ")
     if selected_id == "DONE":
         break
     else: 
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str("${:,.2f}".format(matching_product["price"])))
-
+        selected_ids.append(selected_id)
 #
 # INFO DISPLAY / OUTPUT
 #
+# print(selected_ids)
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str("${:,.2f}".format(matching_product["price"])))
 
 
 print("TOTAL PRICE: " + str("${:,.2f}".format(total_price)))
